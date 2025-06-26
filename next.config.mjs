9294387c133694179e3c682aa507b589d56e75c7
Jotaro1569 <jotarogaming69@gmail.com>
2025-06-26 21:30:23 +0530

    🛠 Switched image config to domains array for Vercel compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-        pathname: '/api/portraits/**', // ✨ Required
-      },
-    ],
+    domains: ['randomuser.me'],
   },
 };
 
